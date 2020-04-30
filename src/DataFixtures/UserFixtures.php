ab644d41733c464fe10e34e92fcd670123ba30d7
Thomas Heuveline <thomasheuveline61@gmail.com>
2020-04-30 17:21:09 +0200

    add security, login, logout

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -12,19 +13,21 @@ class UserFixtures extends Fixture
     private $passwordEncoder;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
-        {
-            $this->passwordEncoder = $passwordEncoder;
-        }
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
 
     public function load(ObjectManager $manager)
     {
         $user = new User();
-
+        $user->setEmail('admin@gmail.com');
+        $user->setRoles(['ROLE_ADMIN']);
         $user->setPassword($this->passwordEncoder->encodePassword(
             $user,
-            'the_new_password'
+            '123456'
         ));
 
+        $manager->persist($user);
         $manager->flush();
     }
 }
--- src/Migrations/Version20200430144601.php
+++ src/Migrations/Version20200430144601.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200409155648 extends AbstractMigration
+final class Version20200430144601 extends AbstractMigration
 {
     public function getDescription() : string
     {
